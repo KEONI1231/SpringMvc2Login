09bcd81361b659afe6a01e861a8f9673983d7f00
keoni <kimkeonhwi991231@gmail.com>
2024-01-06 04:10:01 +0900

    spring interceptor

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -5,7 +5,6 @@ import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.*;
 import javax.validation.Valid;
@@ -81,7 +81,7 @@ public class LoginController
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -97,7 +97,6 @@ public class LoginController
          *  영속쿠키 : 만료 날짜를 입력하면 해당 날짜까지 유지
          *  세션쿠키 : 만료 날짜를 생략하면 브라우저 종료시까지만 유지
          *  쿠키는 임의로 개발자모드에서 변경 가능함.
-         *
          */
         //로그인 성공 처리
         //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성 하여 반환
@@ -106,6 +105,34 @@ public class LoginController
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
     }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+        /**
+         * 쿠키에는 영속쿠키와 세션쿠키 존재
+         *  영속쿠키 : 만료 날짜를 입력하면 해당 날짜까지 유지
+         *  세션쿠키 : 만료 날짜를 생략하면 브라우저 종료시까지만 유지
+         *  쿠키는 임의로 개발자모드에서 변경 가능함.
+         */
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성 하여 반환
+        HttpSession session = request.getSession(true); //false -> 세션이 없으면 새로운 세션을 생셩하지 않는다.
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:"+ redirectURL;
+    }
+
 
     //@PostMapping("/logout")
     public String logout(HttpServletResponse response) {
